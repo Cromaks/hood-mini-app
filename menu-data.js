9332bc4ff2a9a04de5363e8483d19b8fae3eab01
Cromaks <159536653+Cromaks@users.noreply.github.com>
2026-03-18 15:44:23 +0300

    Update menu-data.js

--- menu-data.js
+++ menu-data.js
@@ -22,110 +22,122 @@ window.MENU_DATA = {
 
     "Завтраки": [
       {
-        "name": "Большой завтрак HOOD",
-        "description": "Два яйца-семиминутки, хлеб тартин, песто по-рязански, фета-креметта, хумус из чечевицы и фасоли, домашние вяленые томаты, маринованный перец, битый огурчик, микс-салат, посыпка из чипсов и крошка тартина.",
-        "photo": "big_breakfast_hood.webp",
-        "kcal": 1050.64, "p": 34.12, "f": 51.6, "c": 112.44
+        name: "Большой завтрак HOOD",
+        description: "Два яйца-семиминутки, хлеб тартин, песто, фета-креметта, хумус, томаты, перец, огурчик, салат.",
+        photo: "big_breakfast_hood.webp",
+        kcal: 1050.64, p: 34.12, f: 51.6, c: 112.44
       },
       {
-        "name": "Шакшука",
-        "description": "С овощной сальсой, микс-салатом, дуккой и хлебом тартин.",
-        "photo": "shakshuka.webp",
-        "kcal": 478.63, "p": 19.09, "f": 27.16, "c": 39.45
+        name: "Шакшука",
+        description: "С овощной сальсой и тартином.",
+        photo: "shakshuka.webp",
+        kcal: 478.63, p: 19.09, f: 27.16, c: 39.45
       },
       {
-        "name": "Французский омлет",
-        "description": "С фета-креметтой, микс-салатом и хлебом тартин.",
-        "photo": "french_omelette.webp",
-        "kcal": 659.19, "p": 28.92, "f": 40.75, "c": 44.17
+        name: "Французский омлет",
+        description: "С фета-креметтой и салатом.",
+        photo: "french_omelette.webp",
+        kcal: 659.19, p: 28.92, f: 40.75, c: 44.17
       }
     ],
 
     "Каши": [
       {
-        "name": "Овсяная каша с манго-кремом",
-        "description": "С манго-кремом, сливками и крамблом.",
-        "photo": "kasha_mango_oat.webp",
-        "kcal": 490.68, "p": 6.96, "f": 30, "c": 48.11
+        name: "Овсяная каша с манго-кремом",
+        photo: "kasha_mango_oat.webp",
+        kcal: 490.68, p: 6.96, f: 30, c: 48.11
       },
       {
-        "name": "Рисовая каша с манго-кремом",
-        "description": "С манго-кремом, сливками и крамблом.",
-        "photo": "kasha_mango_rice.webp",
-        "kcal": 503.46, "p": 4.78, "f": 27.75, "c": 58.63
+        name: "Рисовая каша с манго-кремом",
+        photo: "kasha_mango_rice.webp",
+        kcal: 503.46, p: 4.78, f: 27.75, c: 58.63
       },
       {
-        "name": "Овсяная каша с бананами в карамели",
-        "description": "С бананами в карамели и крамблом.",
-        "photo": "kasha_banana_oat.webp",
-        "kcal": 407.37, "p": 6.9, "f": 25.97, "c": 36.5
+        name: "Овсяная каша с бананами в карамели",
+        photo: "kasha_banana_oat.webp",
+        kcal: 407.37, p: 6.9, f: 25.97, c: 36.5
       },
       {
-        "name": "Рисовая каша с бананами в карамели",
-        "description": "С бананами в карамели и крамблом.",
-        "photo": "kasha_banana_rice.webp",
-        "kcal": 418.9, "p": 11.97, "f": 23.4, "c": 47.6
+        name: "Рисовая каша с бананами в карамели",
+        photo: "kasha_banana_rice.webp",
+        kcal: 418.9, p: 11.97, f: 23.4, c: 47.6
       }
     ],
 
     "Сырники": [
       {
-        "name": "Домашние сырники со сметаной",
-        "photo": "homemade_sirniki.webp",
-        "kcal": 457.21, "p": 17.59, "f": 31.57, "c": 25.66
+        name: "Домашние сырники со сметаной",
+        photo: "homemade_sirniki.webp",
+        kcal: 457.21, p: 17.59, f: 31.57, c: 25.66
       }
     ],
 
     "Панкейки": [
       {
-        "name": "Панкейки с бананами в карамели",
-        "photo": "punkake2.webp",
-        "kcal": 654.32, "p": 12.88, "f": 39.48, "c": 61.86
+        name: "Панкейки с бананами в карамели",
+        photo: "punkake2.webp",
+        kcal: 654.32, p: 12.88, f: 39.48, c: 61.86
       }
     ],
 
     "Сэндвичи": [
       {
-        "name": "Сэндвич HOOD",
-        "photo": "sandwich_hood.webp",
-        "kcal": 729.82, "p": 22.52, "f": 42.62, "c": 64.02
+        name: "Сэндвич HOOD",
+        photo: "sandwich_hood.webp",
+        kcal: 729.82, p: 22.52, f: 42.62, c: 64.02
       }
     ],
 
     "Основные блюда": [
       {
-        "name": "Картофельное пюре с говяжьими тефтелями",
-        "photo": "mashed_potato.webp",
-        "kcal": 724.98, "p": 24.69, "f": 59.94, "c": 21.69
+        name: "Картофельное пюре с говяжьими тефтелями",
+        photo: "mashed_potato.webp",
+        kcal: 724.98, p: 24.69, f: 59.94, c: 21.69
       }
     ],
 
     "Кофе": [
-      { "name": "Фильтр", "photo": "filter.webp" },
-      { "name": "Эспрессо", "photo": "espresso.webp" },
-      { "name": "Капучино", "photo": "cappuccino.webp" },
-      { "name": "Латте", "photo": "latte.webp" },
-      { "name": "Флэт-уайт", "photo": "flat_white.webp" },
-      { "name": "Раф-кофе", "photo": "raf_coffee.webp" },
-      { "name": "Бамбл кофе", "photo": "bumble_coffee.webp" },
-      { "name": "Эспрессо-тоник", "photo": "espresso_tonic.webp" },
-      { "name": "Мокко", "photo": "mocha.webp" }
+      { name: "Фильтр", photo: "filter.webp" },
+      { name: "Эспрессо", photo: "espresso.webp" },
+      { name: "Капучино", photo: "cappuccino.webp" },
+      { name: "Латте", photo: "latte.webp" },
+      { name: "Флэт-уайт", photo: "flat_white.webp" },
+      { name: "Раф-кофе", photo: "raf_coffee.webp" },
+      { name: "Бамбл кофе", photo: "bumble_coffee.webp" },
+      { name: "Эспрессо-тоник", photo: "espresso_tonic.webp" },
+
+      { name: "Сырный раф", photo: "cheese_raf.webp" },
+      { name: "Раф копченая груша", photo: "raf_smoked_pear.webp" },
+      { name: "Латте синнабон", photo: "latte_cinnabon.webp" },
+      { name: "Раф солёный арахис", photo: "raf_salted_peanut.webp" },
+      { name: "Мокко", photo: "mocha.webp" },
+      { name: "Фильтр коктейль", photo: "filter_cocktail.webp" },
+      { name: "Банановый раф", photo: "banana_raf.webp" },
+      { name: "Лимонный пирог", photo: "lemon_pie.webp" }
     ],
 
     "Не кофе": [
-      { "name": "Матча-латте", "photo": "matcha_latte.webp" },
-      { "name": "Ходзича", "photo": "hojicha.webp" },
-      { "name": "Какао", "photo": "cocoa.webp" }
+      { name: "Матча-латте", photo: "matcha_latte.webp" },
+      { name: "Ходзича", photo: "hojicha.webp" },
+      { name: "Какао", photo: "cocoa.webp" },
+      { name: "Фисташковое какао", photo: "pistachio_cocoa.webp" },
+      { name: "Банановое какао", photo: "banana_cocoa.webp" }
     ],
 
     "Чай": [
-      { "name": "Цитрусовый пунш", "photo": "citrus_punch.webp" },
-      { "name": "Облепиховый чай", "photo": "sea_buckthorn_tea.webp" }
+      { name: "Ежевичный глинтвейн", photo: "blackberry_mulled.webp" },
+      { name: "Пунш вишня-гибискус", photo: "cherry_hibiscus_punch.webp" },
+      { name: "Цитрусовый пунш", photo: "citrus_punch.webp" },
+      { name: "Облепиховый чай", photo: "sea_buckthorn_tea.webp" },
+      { name: "Домашний сбор с вареньем", photo: "homemade_blend_with_jam.webp" },
+      { name: "Китайский чай", photo: "chinese_tea.webp" }
     ],
 
     "Бабл ти": [
-      { "name": "Сникерс", "photo": "snickers.webp" },
-      { "name": "Матча", "photo": "matcha.webp" }
+      { name: "Сникерс", photo: "snickers.webp" },
+      { name: "Хун-ло сливочная карамель", photo: "hun_lo_creamy_caramel.webp" },
+      { name: "Карамельная далгона", photo: "caramel_dalgona.webp" },
+      { name: "Матча", photo: "matcha.webp" }
     ]
   }
 };
